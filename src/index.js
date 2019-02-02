1e0611883f9b704bff31f35b02036142eacd513d
beerpoldet <poladet.a@gmail.com>
2019-02-02 12:23:22 +0700

    Add redirection for testing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,26 @@ const app = express()
 const port = process.env['PORT'] || 5000
 
 app.get('/', (req, res) => {
-  res.send("Hello, World")
+  res.send('Hello, Mars')
+})
+
+const urlTable = {
+  'aaa': "https://google.com",
+  'bbb': "https://facebook.com",
+  'ccc': "https://twitter.com"
+}
+
+async function findURL(code) {
+  if (!code) return undefined
+  return urlTable[code]
+}
+
+app.get('/:code', async (req, res) => {
+  const url = await findURL(req.params['code'])
+  if (url)
+    res.redirect(url)
+  else
+    res.send('URL Not Found')
 })
 
 app.listen(port, () => {
